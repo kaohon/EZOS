0ab69ae0dffee6a84a994c47ec3908cf08592e95
kaohon <gudoumaki@gmail.com>
2023-10-26 00:28:51 +0900

    main

--- timer.js
+++ timer.js
@@ -3,24 +3,48 @@ var countdown = function(){
 }
 
 y = 0;
+x = 0;
+n = 0;
+z = 0;
 function hour1() {
+    x = x + 1;
     y = y + 600000;
-    gentime.innerHTML = y;
+    hourtime.innerHTML = x + "時";
 }
 
 function minute1() {
+    n = n + 1;
     y = y + 60000;
-    gentime.innerHTML = y;
+    mintime.innerHTML = n + "分";
 }
 
 function second1() {
+    z = z + 1;
     y = y + 1000;
-    gentime.innerHTML = y;
+    sectime.innerHTML = z + "秒";
+}
+
+function minus_hour1() {
+    x = x - 1;
+    y = y - 600000;
+    hourtime.innerHTML = x + "時";
+}
+
+function minus_minute1() {
+    n = n - 1;
+    y = y - 60000;
+    mintime.innerHTML = n + "分";
+}
+
+function minus_second1() {
+    z = z - 1;
+    y = y - 1000;
+    sectime.innerHTML = z + "秒";
 }
 
 function down() {
 setTimeout(countdown, y);
-for (let i = y; i <= 1000; i++) {
-    gentime.innerHTML = i - 1;
-}
+    hourtime.innerHTML = i;
+    mintime.innerHTML = i;
+    sectime.innerHTML = i;
 }
